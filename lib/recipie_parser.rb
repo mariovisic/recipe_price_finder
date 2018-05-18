6de22f1913b04d00622dfe2462abda31c8a9e412
Mario Visic <mario@mariovisic.com>
2018-05-18 22:40:07 +1000

    Start work on ability to identify ingredient items
    I've made progress on the ingredient parser, it's not functional yet but
    I'm happy with this basic structure for now.

--- lib/recipie_parser.rb
+++ lib/recipie_parser.rb
@@ -1,2 +1,88 @@
+require 'nokogiri'
+require 'ostruct'
+
+class Recipe < OpenStruct
+end
+
+class IngredientItem < OpenStruct
+  TABLE_SPOON = 'tbsp'
+
+  def to_s
+    "#{quantity} #{quantity_unit} of #{name}"
+  end
+end
+
+
 class RecipieParser
+  def self.parse(file)
+    file.respond_to?(:read) ? new(file.read).parse : new(file).parse
+  end
+
+  def initialize(html)
+    @document = Nokogiri::HTML(html)
+  end
+  private_class_method :new
+
+  def parse
+    rec = Recipe.new(ingredient_items: []).tap do |recipie|
+      ingredient_elements.each do |ingredient_element|
+        recipie.ingredient_items.push(IngredientItemParser.parse(ingredient_element))
+      end
+    end
+  end
+
+  private
+
+  def ingredient_elements
+    @document.xpath('//li[contains(@class, "ingredient")]')
+  end
+end
+
+class IngredientItemParser
+  def self.parse(html_element)
+    new(html_element).parse
+  end
+
+  def initialize(html_element)
+    @html_element = html_element
+  end
+
+  def parse
+    IngredientItem.new(element: @html_element, quantity: quantity, quantity_unit: quantity_unit, name: name)
+  end
+
+  private
+
+  def name
+    if quantity_string
+      line_with_quantity.sub(quantity_string, '').strip.capitalize
+    end
+  end
+
+  def quantity
+    if quantity_string
+      quantity_string.split(' ').first.to_i
+    end
+  end
+
+  def quantity_unit
+    if quantity_string
+      quantity_string.split(' ').last
+    end
+  end
+
+
+  def quantity_string
+    @quantity ||= begin
+      if (@html_element.text.match /\d tbsp/i)
+        @html_element.text.match(/\d tbsp/i)[0]
+      end
+    end
+  end
+
+  def line_with_quantity
+    @html_element.text.split("\n").detect do |line|
+      line.match?(/\d tbsp/i)
+    end
+  end
 end
--- spec/lib/recipie_parser_spec.rb
+++ spec/lib/recipie_parser_spec.rb
@@ -1,11 +1,40 @@
 require 'spec_helper'
 require File.dirname(__FILE__) + '/../../lib/recipie_parser'
 
-class Recipie
+RSpec.describe RecipieParser do
+  let(:recipe_html_file) { File.read(File.dirname(__FILE__) + '/../support/example_recipie_webpages/bbc_good_food_chickpea_curry.html') }
+
+  describe '.parse' do
+    it 'returns a Recipie object' do
+      expect(RecipieParser.parse(recipe_html_file)).to be_a(Recipe)
+    end
+
+    it 'correctly sets the number of ingredients on the recipie' do
+      expect(RecipieParser.parse(recipe_html_file).ingredient_items.count).to eq(12)
+    end
+  end
 end
 
-RSpec.describe RecipieParser do
+
+RSpec.describe IngredientItemParser do
+
+  let(:html) { %{<li class="ingredients-list__item" itemprop="ingredients" content="1 tbsp olive oil">1 tbsp <a href="/glossary/olive-oil" class="ingredients-list__glossary-link" data-tooltip-content="#ingredients-glossary &gt; article" data-tooltip-width="350" data-tooltip-hide-delay="200" data-tooltip-flyout="true">olive oil</a><span class="ingredients-list__glossary-element" id="ingredients-glossary"><article id="node-259466" role="main" class="node node-glossary-item node-teaser node-teaser clearfix main row grid-padding"><div class="node-image"> <a href="/glossary/olive-oil"><img src="//www.bbcgoodfood.com/sites/default/files/styles/bbcgf_thumbnail_search/public/glossary/olive-oil.jpg?itok=n4dxcprV" width="100" height="100" alt="olive oil" title="olive oil"></a>
+</div>
+<h2 class="node-title node-glossary-title"><a href="/glossary/olive-oil">Olive oil</a></h2> <span class="fonetic text-style-alt">ol-iv oyl</span><p>Probably the most widely-used oil in cooking, olive oil is pressed from fresh olives. It's…</p> </article></span>
+</li>} }
+
   describe '.parse' do
-    expect(RecipieParser.parse(File.open(File.dirname(__FILE__) + '/../support/example_recipie_webpages/bbc_good_food_chickpea_curry.html'))).to be_a(Recipie)
+    it 'returns an IngredientItem object' do
+      expect(IngredientItemParser.parse(Nokogiri::XML::Document.parse(html))).to be_a(IngredientItem)
+    end
+
+    it 'correctly extracts the quantity of the ingredients' do
+      expect(IngredientItemParser.parse(Nokogiri::XML::Document.parse(html)).quantity).to eq(1)
+      expect(IngredientItemParser.parse(Nokogiri::XML::Document.parse(html)).quantity_unit).to eq(IngredientItem::TABLE_SPOON)
+    end
+
+    it 'correctly extracts the name of the ingredients' do
+      expect(IngredientItemParser.parse(Nokogiri::XML::Document.parse(html)).name).to eq('Olive oil')
+    end
   end
 end
